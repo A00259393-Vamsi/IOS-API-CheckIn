4e2432110b8ac4cc4a202fcad06450d43c35ae58
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-10-18 23:35:41 -0400

    Added methods for different apis

--- swapi/SWAPI_Helper.swift
+++ swapi/SWAPI_Helper.swift
@@ -8,8 +8,10 @@
 import Foundation
 
 class SWAPI_Helper{
-    static private let urlString = "https://swapi.dev/api/people/1";
-    
+    static private let urlStringContests = "https://kontests.net/api/v1/all";
+    static private let urlStringCurrency = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json";
+	static private let urlStringJobs = "https://www.arbeitnow.com/api/job-board-api";
+	
     static private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
@@ -18,9 +20,67 @@ class SWAPI_Helper{
     static public func fetchDir(){
         print(#function)
         guard
-            let url = URL(string: urlString)
+            let url = URL(string: urlStringContests)
         else{
-            preconditionFailure("was not able to convert string to url: \(urlString)")
+            preconditionFailure("was not able to convert string to url: \(urlStringContests)")
+        }
+        
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) {
+            data, response, error in
+            
+            if let data = data {
+                do{
+                    let jsonData = try JSONSerialization.jsonObject(with: data)
+                    
+                    print(jsonData)
+                }catch let err{
+                    print("\(err)")
+                }
+            } else if let error = error {
+                print("error with data task, fetchDir: \(error)")
+            } else {
+                print("something went wrong when fetching the directory: \(String(describing: response))")
+            }
+        }
+        task.resume()
+    }
+	
+	static public func fetchCurrency(){
+        print(#function)
+        guard
+            let url = URL(string: urlStringCurrency)
+        else{
+            preconditionFailure("was not able to convert string to url: \(urlStringCurrency)")
+        }
+        
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) {
+            data, response, error in
+            
+            if let data = data {
+                do{
+                    let jsonData = try JSONSerialization.jsonObject(with: data)
+                    
+                    print(jsonData)
+                }catch let err{
+                    print("\(err)")
+                }
+            } else if let error = error {
+                print("error with data task, fetchDir: \(error)")
+            } else {
+                print("something went wrong when fetching the directory: \(String(describing: response))")
+            }
+        }
+        task.resume()
+    }
+	
+	static public func fetchJobs(){
+        print(#function)
+        guard
+            let url = URL(string: urlStringJobs)
+        else{
+            preconditionFailure("was not able to convert string to url: \(urlStringJobs)")
         }
         
         let request = URLRequest(url: url)
